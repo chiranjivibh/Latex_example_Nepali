e99bdeaddbcc22508b88602d93c3431d0da84ca1
Chiranjivi Bhattarai <chiranjivibh@gmail.com>
2022-08-17 17:11:48 -0700

    Add files via upload

---
+++ Compile_withPython.py
@@ -0,0 +1,8 @@
+# 2nd way
+import os
+
+tex_file_name = "Main_latex.tex";
+filename= "Tryal_unicode_nepali"
+os.system("devnag " + "Tryal_unicode_nepali")
+#os.system("pdflatex " + tex_file_name);
+os.system("pdflatex " + filename+".tex");

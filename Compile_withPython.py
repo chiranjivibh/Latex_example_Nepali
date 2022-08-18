62206061afdefe0be44cfcbc49c4506c35fcdf9d
Chiranjivi Bhattarai <chiranjivibh@gmail.com>
2022-08-17 17:12:40 -0700

    CB updated

--- Compile_withPython.py
+++ Compile_withPython.py
@@ -1,8 +1,4 @@
-# 2nd way
 import os
-
-tex_file_name = "Main_latex.tex";
 filename= "Tryal_unicode_nepali"
-os.system("devnag " + "Tryal_unicode_nepali")
-#os.system("pdflatex " + tex_file_name);
+os.system("devnag " + filename);
 os.system("pdflatex " + filename+".tex");
